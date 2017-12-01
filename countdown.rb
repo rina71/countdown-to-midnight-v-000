95efa8e8a3d516e987bfd1cfce7398ac090112c1
Yurina Shin <yurinas@gmail.com>
2017-12-01 23:30:16 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown(number)
-  while number < 11 
-  "#{number} SECOND(S)!"
+  while number < 11
+  puts "#{number} SECOND(S)!"
   number = number -= 1
 end
   "HAPPY NEW YEAR!"
