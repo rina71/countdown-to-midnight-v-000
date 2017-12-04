b2bef5e5076e6c6c337d43a31285449f848eb773
Yurina Shin <yurinas@gmail.com>
2017-12-04 20:29:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+# require 'pry'
 
-def countdown
+def countdown(x)
+x = 10
+  while 0 < x &&  x < 11
+
+  puts "#{x} SECOND(S)!"
+  x -= 1
+  # binding.pry
+end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(y)
+  y = sleep 5
 end
